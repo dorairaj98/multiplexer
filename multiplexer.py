ab4f6901f6a2aa53b3e39a69328c6ec56228c34e
dorairaj98 <69347899+dorairaj98@users.noreply.github.com>
2020-12-01 02:43:05 +0530

    adding library file

---
+++ multiplexer.py
@@ -0,0 +1,9 @@
+class myplexer:
+   def route(self, arg):
+      def inner(func):
+         user_input = input("enter Input: ")
+         if user_input == arg:
+            print(func())
+            func()
+      return inner
+
